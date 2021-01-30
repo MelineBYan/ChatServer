717410bbbc9a89292200dcff28046a3881c3a0b1
MelineBYan <77862334+MelineBYan@users.noreply.github.com>
2021-01-30 04:49:42 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoDBUrl: "mongodb+srv://chatDBuser:chatDBpass@cluster0.mufv7.mongodb.net/class-chat?retryWrites=true&w=majority",
+    mongoDBUrl: process.env.MONGOBB_URL,
     port: process.env.PORT || 3000
 }
